770b3f33d2b5f67dbee0131997b128593c5209be
Konstantin Golovanov <KGolovanov@itkey.com>
2024-06-19 15:05:57 +0200

    Level07 updated comments

--- level07/main.c
+++ level07/main.c
@@ -3,11 +3,14 @@
 #include <string.h>
 
 int clear_stdin() {
-    int result;
+    char result = 0;
 
-    do
+    while (1)
+    {
         result = getchar();
-    while (result != '\n' && result != -1);;
+        if (result == '\n' || result == -1)
+            break;
+    }
     return result;
 }
 
@@ -21,46 +24,49 @@ unsigned int get_unum() {
     return input;
 }
 
-int store_number(unsigned int *tab) {
-    unsigned int inp;
-    unsigned int ind;
+int store_number(unsigned int *data) {
+    unsigned int num = 0;
+    unsigned int i = 0;
 
     printf(" Number: ");
-    inp = get_unum();
+    num = get_unum();
+
     printf(" Index: ");
-    ind = get_unum();
-    if (ind % 3 == 0 || inp >> 24 == 183) {
+    i = get_unum();
+
+    if (i % 3 == 0 || num >> 24 == 183) {
         puts(" *** ERROR! ***");
         puts("   This ind is reserved for wil!");
         puts(" *** ERROR! ***");
         return 1;
     } else {
-        tab[ind] = inp;
+        data[i] = num;
         return 0;
     }
 }
 
-int read_number(unsigned int *tab) {
-    unsigned int inp;
+int read_number(unsigned int *data) {
+    unsigned int i;
 
     printf(" Index: ");
-    inp = get_unum();
-    printf(" Number at data[%u] is %u\n", inp, tab[inp]);
+    i = get_unum();
+    printf(" Number at data[%u] is %u\n", i, data[i]);
     return 0;
 }
 
 int main(int argc, const char **argv, const char **envp) {
     int result;
-    char buff[20];
-    unsigned int tab[100];
-    int i;
+    char input[20];
+    unsigned int data[100];
+
     char *argv_p;
     char *envp_p;
 
-    memset(buff, 0, 20);
-    memset(tab, 0, 400);
+    // Clearing input and data
+    memset(input, 0, 20);
+    memset(data, 0, 400);
 
-    i = 0;
+    // Clearing argv and envp
     argv_p = (char *) argv;
     while (*argv_p) {
         memset(argv_p, 0, strlen(argv_p));
@@ -72,9 +78,10 @@ int main(int argc, const char **argv, const char **envp) {
         ++envp_p;
     }
 
+    // Print welcome message
     puts(
             "----------------------------------------------------\n"
-            "  Welcome to wil'tab crappy result storage service!   \n"
+            "  Welcome to wil'data crappy result storage service!   \n"
             "----------------------------------------------------\n"
             " Commands:                                          \n"
             "    store - store a result into the data storage    \n"
@@ -83,21 +90,23 @@ int main(int argc, const char **argv, const char **envp) {
             "----------------------------------------------------\n"
             "   wil has reserved some storage :>                 \n"
             "----------------------------------------------------\n");
+
+    // Main loop
     while (1) {
         printf("Input command: ");
         result = 1;
-        fgets(buff, 20, stdin);
-        buff[strlen(buff) - 2] = 0;
-        if (!memcmp(tab, "store", 5)) {
-            result = store_number(tab);
-        } else if (!memcmp(tab, "read", 4)) {
-            result = read_number(tab);
-        } else if (!memcmp(tab, "quit", 4))
+        fgets(input, 20, stdin);
+        input[strlen(input) - 2] = '\0';
+        if (!memcmp(data, "store", 5))
+            result = store_number(data);
+        else if (!memcmp(data, "read", 4))
+            result = read_number(data);
+        else if (!memcmp(data, "quit", 4))
             return 0;
         if (result)
-            printf(" Failed to do %s command\n", buff);
+            printf(" Failed to do %s command\n", input);
         else
-            printf(" Completed %s command successfully\n", buff);
-        memset(buff, 0, 20);
+            printf(" Completed %s command successfully\n", input);
+        memset(input, 0, 20);
     }
 }
